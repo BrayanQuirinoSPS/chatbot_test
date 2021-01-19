53b6299b00b46d4a50da9b65d068a8ae548dc306
BrayanQuirinoSPS <bquirino@spsolutions.com.mx>
2021-01-19 16:05:07 -0600

    Pretty JSON

--- src/cmain.py
+++ src/cmain.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+import json
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -9,4 +10,7 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
+	print(json.dumps(json.loads(message), indent=2, sort_keys=True))
 	bot.reply_to(message, message.text)
+
+bot.polling()
